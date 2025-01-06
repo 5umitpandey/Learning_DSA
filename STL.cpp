a2dbb0632f20317e4cd9c2f521bd9c0d97789e8e
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-01-06 07:48:52 +0530

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+/*
+Pairs
+Vector
+*/
 
 
 // Pairs
@@ -22,10 +25,44 @@ void Pairs()
     cout << arr[1].second;
 }
 
+//Vector
+void Vectors()
+{
+    vector<int> v;
+
+    v.push_back(1);     //{1}
+    v.emplace_back(2);  //{1, 2}
+
+    //Vector Pair
+    vector< pair<int, int> > vec;
+
+    vec.push_back({1, 2});  // { {1, 2} }
+    vec.emplace_back(3, 4);  // { {1,2}, {3,4} }
+
+    //Declare vector with 5 instances of '20'
+    vector<int> v1(5, 20);
+
+    //Declare vector with 5 instances of 'random values'
+    vector<int> v2(5);
+
+    //Copy v1 into v3
+    vector<int> v3(v1);
+
+
+    vector<int>::iterator it = v.begin();
+
+    cout << *(it); //Prints 1
+
+    vector<int>::iterator it_e = v.end();
+    vector<int>::reverse_iterator it_re = v.rend();
+    vector<int>::reverse_iterator it_rb = v.rbegin();
+
+}
+
 
 int main()
 {
-    Pairs();
-
+    //Pairs();
+    Vectors();
     return 0;
 }
