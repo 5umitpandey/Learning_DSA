330f680eaeaf3edc8b66b22c9cd7c8d84df4f94d
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-01-27 23:59:47 +0530

    Update 1D_To_2D.cpp

--- 1D_To_2D.cpp
+++ 1D_To_2D.cpp
@@ -8,6 +8,7 @@ vector<vector<int>> construct2DArray(vector<int>& original, int m, int n)
         }
 
         vector<vector<int>> result(m, vector<int>(n));
+        
         for (int i = 0; i < original.size(); ++i) 
         {
             result[i / n][i % n] = original[i];
