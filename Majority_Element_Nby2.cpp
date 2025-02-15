481d597ecba0f5a02b426312e069c2761e58a25b
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-02-15 18:13:14 +0530

    Update Majority_Element_Nby2.cpp

--- Majority_Element_Nby2.cpp
+++ Majority_Element_Nby2.cpp
@@ -1,51 +1,51 @@
 //https://leetcode.com/problems/majority-element/
 
-    //Approach 1 | BruteForce
-    int majorityElementBruteForce(vector<int>& nums) 
-    {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) 
-        {
-            int count = 0;
-            for (int j = 0; j < n; j++) 
-            {
-                if (nums[j] == nums[i]) 
-                {
-                    count++;
-                }
-            }
-            if (count > n / 2) 
-            {
-                return nums[i];
-            }
-        }
-        return -1;
-    }
+ //Approach 1 | BruteForce
+    // int majorityElement(vector<int>& nums) 
+    // {
+    //     int n = nums.size();
+    //     for (int i = 0; i < n; i++) 
+    //     {
+    //         int count = 0;
+    //         for (int j = 0; j < n; j++) 
+    //         {
+    //             if (nums[j] == nums[i]) 
+    //             {
+    //                 count++;
+    //             }
+    //         }
+    //         if (count > n / 2) 
+    //         {
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
     //O(n^n)
     //O(1)
 
 
     //Approach 2 | Hashmap
-    int majorityElementHashMap(vector<int>& nums) 
-    {
-        unordered_map<int, int> counts;
-        int n = nums.size();
-        for (int num : nums) 
-        {
-            counts[num]++;
-            if (counts[num] > n / 2) 
-            {
-                return num;
-            }
-        }
-        return -1;
-    }
+    // int majorityElement(vector<int>& nums) 
+    // {
+    //     unordered_map<int, int> counts;
+    //     int n = nums.size();
+    //     for (int num : nums) 
+    //     {
+    //         counts[num]++;
+    //         if (counts[num] > n / 2) 
+    //         {
+    //             return num;
+    //         }
+    //     }
+    //     return -1;
+    // }
     //O(n)
     //O(n)
 
 
     //Approach 3 | Boyer-Moore Voting Algorithm
-    int majorityElementBoyerMoore(vector<int>& nums) 
+    int majorityElement(vector<int>& nums) 
     {
         int candidate = 0;
         int count = 0;
@@ -59,3 +59,5 @@
         }
         return candidate;
     }
+    //O(n)
+    //O(1)
