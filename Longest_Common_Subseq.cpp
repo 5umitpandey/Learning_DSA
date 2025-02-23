ed9756c7d09c19a61fad899a0b230a54b5cce8f5
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-02-23 15:29:50 +0530

    Update Longest_Common_Subseq.cpp

--- Longest_Common_Subseq.cpp
+++ Longest_Common_Subseq.cpp
@@ -1,3 +1,81 @@
 /*
 Given 2 strings, find longest common subsequence.
 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int longestCommonSubsequence(const string& text1, const string& text2) 
+{
+    int m = text1.size();
+    int n = text2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Filling the DP table
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+// Function to print the longest common subsequence
+string getLCS(const string& text1, const string& text2) 
+{
+    int m = text1.size();
+    int n = text2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Filling the DP table
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // Reconstructing the LCS from the DP table
+    string lcs;
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (text1[i - 1] == text2[j - 1]) {
+            lcs.push_back(text1[i - 1]);
+            --i;
+            --j;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            --i;
+        } else {
+            --j;
+        }
+    }
+
+    reverse(lcs.begin(), lcs.end());
+    return lcs;
+}
+
+int main() 
+{
+    string text1 = "AGGTAB";
+    string text2 = "GXTXAYB";
+
+    int lcsLength = longestCommonSubsequence(text1, text2);
+    string lcs = getLCS(text1, text2);
+
+    cout << "Length of Longest Common Subsequence: " << lcsLength << endl;
+    cout << "Longest Common Subsequence: " << lcs << endl;
+
+    return 0;
+}
