57a65f3f5cb8f2cb0a2ce531bbe2831a6a9ca9f9
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-03-05 19:09:40 +0530

    Update Palimdromic_Substrings.cpp

--- Palimdromic_Substrings.cpp
+++ Palimdromic_Substrings.cpp
@@ -30,3 +30,49 @@ int countSubstrings(string s)
 
 //O(n^3)
 //O(1)
+
+
+
+//Approach 2 | DP
+int countSubstrings(string s) 
+{
+    int n = s.length();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int count = 0;
+
+    // Every single character is a palindrome
+    for (int i = 0; i < n; ++i) 
+    {
+        dp[i][i] = true;
+        count++;
+    }
+
+    // Check for palindromic substrings of length 2
+    for (int i = 0; i < n - 1; ++i) 
+    {
+        if (s[i] == s[i + 1]) 
+        {
+            dp[i][i + 1] = true;
+            count++;
+        }
+    }
+
+    // Check for palindromic substrings of length 3 or more
+    for (int len = 3; len <= n; ++len) 
+    {
+        for (int i = 0; i <= n - len; ++i) 
+        {
+            int j = i + len - 1;
+            if (s[i] == s[j] && dp[i + 1][j - 1]) 
+            {
+                dp[i][j] = true;
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+//O(n^2)
+//O(n^2)
+
