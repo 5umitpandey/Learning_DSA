80dfefd4e287be1a22a3bac66b1cb50f374ef23d
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-03-10 21:41:06 +0530

    Update Climbing_Stairs.cpp

--- Climbing_Stairs.cpp
+++ Climbing_Stairs.cpp
@@ -5,3 +5,21 @@ int climbStairs(int n)
         if (n == 0 || n == 1) return 1;
         return climbStairs(n - 1) + climbStairs(n - 2);    
     }
+    //O(2^n)
+    //O(n)
+
+    int climbStairs(int n) 
+    {
+        if (n == 0 || n == 1) return 1;
+
+        int prev1 = 1, prev2 = 1;
+        for (int i = 2; i <= n; ++i) 
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    //O(n)
+    //O(n)
