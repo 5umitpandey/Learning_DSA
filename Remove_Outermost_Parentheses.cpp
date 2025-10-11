0d98c416e97d320f0b6a121da2e5a1ae029c0307
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-10-11 16:36:23 +0530

    Create Remove_Outermost_Parentheses.cpp

---
+++ Remove_Outermost_Parentheses.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/remove-outermost-parentheses/description/
+
+string removeOuterParentheses(string s) 
+    {
+        string result = "";  
+        
+        int level = 0;       
+
+        for (char ch : s) 
+        {
+            if (ch == '(') 
+            {
+                if (level > 0) result += ch;
+                level++;  
+            } 
+            else if (ch == ')') 
+            {
+                level--;  
+                if (level > 0) result += ch;
+            }
+        }
+
+        return result;    
+    }
