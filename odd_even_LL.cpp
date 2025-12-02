dbcd67c84e8b23443ed0a148430e11a7c4d2a2a2
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-12-02 19:22:20 +0530

    Refactor oddEvenList function formatting

--- odd_even_LL.cpp
+++ odd_even_LL.cpp
@@ -1,21 +1,21 @@
 //https://leetcode.com/problems/odd-even-linked-list/
 
 ListNode* oddEvenList(ListNode* head) 
+{
+    if (head == nullptr || head->next == nullptr) return head;
+
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+
+    while (even != nullptr && even->next != nullptr) 
     {
-        if (head == nullptr || head->next == nullptr) return head;
-
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-
-        while (even != nullptr && even->next != nullptr) 
-        {
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = even->next->next;
-            even = even->next;
-        }
-
-        odd->next = evenHead; // Connect odd list to even list
-        return head;        
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = even->next->next;
+        even = even->next;
     }
+
+    odd->next = evenHead; // Connect odd list to even list
+    return head;        
+}
