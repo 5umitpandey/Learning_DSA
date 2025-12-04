cebbf8f45f16c4aeea80686de319353ee2e5e381
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-12-04 17:14:42 +0530

    Add deleteMiddle function for linked list
    Implement function to delete the middle node of a linked list.

---
+++ Delete_Mid_Node_LL.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+ListNode* deleteMiddle(ListNode* head) 
+    {
+        if (!head || !head->next) return nullptr;
+
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) 
+        {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (prev) prev->next = slow->next;  // Remove middle node
+        delete slow;  // Free memory
+
+        return head;    
+    }
