737d9ea0c73c7818c439624fed8430f5aad66124
Sumit Pandey <68957789+5umitpandey@users.noreply.github.com>
2025-12-06 13:26:32 +0530

    Create Intersection_of_2LL.cpp

---
+++ Intersection_of_2LL.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        ListNode *temp;
+		while(headA != NULL)
+        {
+			temp = headB;
+			while(temp != NULL)
+            {
+				if(headA == temp)
+                {
+					return headA;
+				}
+				temp = temp -> next;
+			}
+			headA = headA -> next;
+		}
+		return NULL;    
+    }
